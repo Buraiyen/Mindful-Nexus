2a0a4c7996a4424914b49d5b03132d5ed2e15cd9
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-03 15:40:19 -0700

    :sparkles: feat: timeline test data

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { TimelineItem } from './timeline-item/timeline-item.component';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.scss']
+  styleUrls: ['./timeline.component.scss'],
 })
 export class TimelineComponent {
-
+  timelineItems: TimelineItem[] = [
+    {
+      id: 1,
+      title: 'Psychology of Money',
+      date: new Date('10/02/98').toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: 'short',
+      }),
+      category: 'book',
+    },
+    {
+      id: 2,
+      title: 'This is Our Fight',
+      date: new Date('03/02/21').toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: 'short',
+      }),
+      category: 'podcast',
+    },
+  ];
 }
