cba3cc23a48eb116a10a2dca5cbbf623757d4486
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-03 12:30:51 -0700

    feat: TimelineItem text

--- src/app/timeline-item/timeline-item.component.ts
+++ src/app/timeline-item/timeline-item.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface TimelineItem {
+  id: number;
+  title: string;
+  date: Date;
+  category: string;
+}
+
 @Component({
   selector: 'app-timeline-item',
   templateUrl: './timeline-item.component.html',
-  styleUrls: ['./timeline-item.component.scss']
+  styleUrls: ['./timeline-item.component.scss'],
 })
 export class TimelineItemComponent {
-
+  timelineItem: TimelineItem = {
+    id: 1,
+    title: 'Psychology of Money',
+    date: new Date('10/02/98'),
+    category: 'book',
+  };
 }
