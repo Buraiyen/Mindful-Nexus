54911b1ea5cfb0dda9f98456771f6388e1d1542a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-02 19:47:51 -0700

    feat: initial quote

--- src/app/home-page/home-page-header/home-page-header.component.ts
+++ src/app/home-page/home-page-header/home-page-header.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-page-header',
   templateUrl: './home-page-header.component.html',
-  styleUrls: ['./home-page-header.component.scss']
+  styleUrls: ['./home-page-header.component.scss'],
 })
 export class HomePageHeaderComponent {
+  sayings = [
+    {
+      regular: 'Believe in yourself,',
+      bold: 'always',
+    },
+  ];
 
+  randomIndex = Math.floor(Math.random() & this.sayings.length);
+  retrieved = this.sayings[this.randomIndex];
+
+  selectedString = `${this.retrieved.regular} ${this.retrieved.bold}`;
 }
