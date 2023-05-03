c7a4062c4df4353f38386f116fb98de592e14cfe
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-02 20:02:50 -0700

    feat: more sayings, fixed randomIndex

--- src/app/home-page/home-page-header/home-page-header.component.ts
+++ src/app/home-page/home-page-header/home-page-header.component.ts
@@ -16,8 +16,32 @@ export class HomePageHeaderComponent {
       regular: 'Believe in yourself,',
       bold: 'always',
     },
+    {
+      regular: 'Dream big,',
+      bold: 'work hard',
+    },
+    {
+      regular: 'Be the',
+      bold: 'change',
+    },
+    {
+      regular: 'Create your own',
+      bold: 'happiness',
+    },
+    {
+      regular: 'Live life with',
+      bold: 'purpose',
+    },
+    {
+      regular: 'Embrace your',
+      bold: 'inner strength',
+    },
+    {
+      regular: 'Treat others with',
+      bold: 'kindness',
+    },
   ];
 
-  randomIndex = Math.floor(Math.random() & this.sayings.length);
+  randomIndex = Math.floor(Math.random() * this.sayings.length);
   saying = this.sayings[this.randomIndex];
 }
