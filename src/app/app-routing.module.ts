c7f275b57f5e7255aa766ddb679fd8fdc48781e5
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-05 12:53:19 -0700

    :sparkles: feat: DynamiccPage routing function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PAGE_DATA } from './dynamic-page/page-data';
+import { DynamicPageComponent } from './dynamic-page/dynamic-page.component';
 
 const routes: Routes = [];
 
+PAGE_DATA.forEach((page) => {
+  routes.push({
+    path: page.id,
+    component: DynamicPageComponent,
+    data: page,
+  });
+});
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
