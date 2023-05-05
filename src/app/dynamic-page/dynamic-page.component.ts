e85d9c94648b6882e963b8e6d4dcd6e4af18fd42
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-05 12:55:28 -0700

    :sparkles: feat: passed props to dynamic page

--- src/app/dynamic-page/dynamic-page.component.ts
+++ src/app/dynamic-page/dynamic-page.component.ts
@@ -1,14 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dynamic-page',
   templateUrl: './dynamic-page.component.html',
   styleUrls: ['./dynamic-page.component.scss'],
 })
-export class DynamicPageComponent {
-  @Input() id?: string;
-  @Input() title?: string;
-  @Input() date?: Date;
-  @Input() category?: string;
-  @Input() content?: [];
+export class DynamicPageComponent implements OnInit {
+  pageData: any;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.pageData = this.route.snapshot.data;
+  }
 }
