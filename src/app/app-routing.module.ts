5f46a3ebff3da9aaeb34e052df3284238bf7a41e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-05 13:00:35 -0700

    :sparkles: feat: route for HomePageComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PAGE_DATA } from './dynamic-page/page-data';
+import { HomePageComponent } from './home-page/home-page.component';
 import { DynamicPageComponent } from './dynamic-page/dynamic-page.component';
 
 const routes: Routes = [];
 
+routes.push({
+  path: '',
+  component: HomePageComponent,
+});
+
+// adding dynamic routes for dynamic pages
 PAGE_DATA.forEach((page) => {
   routes.push({
     path: page.id,
