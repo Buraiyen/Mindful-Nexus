1cfa3b9a5c32ad6c2a4cd571532d7ac8e2e3c8aa
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-05 11:37:07 -0700

    :sparkles: feat: mock page data

---
+++ src/app/dynamic-page/page-data.ts
@@ -0,0 +1,12 @@
+export const PAGE_DATA = [
+  {
+    id: 'psychology-of-money',
+    title: 'Psychology of Money',
+    date: new Date('10/02/98'),
+    category: 'book',
+    content: [
+      'Psychology of money is about money',
+      'Here is the second paragraph',
+    ],
+  },
+];
