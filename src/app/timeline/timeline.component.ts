f843afeaa1b290fc1a327d457516fb1a0fabca5e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-05 16:13:31 -0700

    :recycle: refactor: import page data

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TimelineItem } from './timeline-item/timeline-item';
+import { PAGE_DATA } from '../dynamic-page/page-data';
 
 @Component({
   selector: 'app-timeline',
@@ -7,18 +8,5 @@ import { TimelineItem } from './timeline-item/timeline-item';
   styleUrls: ['./timeline.component.scss'],
 })
 export class TimelineComponent {
-  timelineItems: TimelineItem[] = [
-    {
-      id: 1,
-      title: 'Psychology of Money',
-      date: new Date('10/02/98'),
-      category: 'book',
-    },
-    {
-      id: 2,
-      title: 'This is Our Fight',
-      date: new Date('03/02/21'),
-      category: 'podcast',
-    },
-  ];
+  timelineItems: TimelineItem[] = PAGE_DATA;
 }
