c285405b91c6dd1350492b6a548930cfcb7e4af8
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-06 12:46:00 -0700

    :sparkles: feat: external links in data

--- src/app/data/page-data.ts
+++ src/app/data/page-data.ts
@@ -7,7 +7,7 @@ export const PAGE_DATA = [
     content: [
       '"The Psychology of Money" is a book by Morgan Housel that explores the complex relationship between people and money. The book focuses on the psychology and behavior of individuals, rather than financial theory or investment strategies. It examines the various ways that people think about money, and how these beliefs and behaviors impact their financial decisions',
     ],
-    link: '',
+    link: 'https://www.amazon.com/Psychology-Money-Timeless-Lessons-Happiness/dp/B08D9WJ9G8/ref=sr_1_1?crid=1X5IUGMH00DRM&keywords=the+psychology+of+money+morgan+housel&qid=1683402314&sprefix=psycholoig%2Caps%2C203&sr=8-1',
   },
   {
     id: 'consider-before-consuming',
@@ -18,6 +18,6 @@ export const PAGE_DATA = [
       'Consider Before Consuming is a podcast from "Fight the New Drug," a non-profit organization that aims to raise awareness about the harmful effects of pornography and help individuals to break free from addiction.',
       'The podcast features interviews with experts in the fields of psychology, addiction recovery, and social science, as well as personal stories from individuals who have struggled with pornography addiction. The episodes cover a range of topics related to pornography, including its impact on the brain, the ways it can harm relationships, and the challenges of breaking free from addiction',
     ],
-    link: '',
+    link: 'https://considerbeforeconsumingpodcast.com/',
   },
 ];
